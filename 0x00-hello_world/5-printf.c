f98f4a2fdda5b02b12491ff24ebe6937e8ee8b80
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-07 12:12:48 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: A C program that print with printf function 
- *
+ * Description: A C program that prints with printf function. *
  * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
