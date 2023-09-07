2afeae21b06dd1c826630c994d13e1871ae9d612
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-07 10:20:06 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h
 
-voidt main(void)
+/**
+ * main -Entry point
+ *
+ * Description:A C program that prints with put function
+ *
+ * Return:always 0 
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
