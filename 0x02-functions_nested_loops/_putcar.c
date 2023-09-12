244485ad83409a73138c5eae6e1a8d1789ff55cf
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-12 07:16:36 -0700

    a

---
+++ 0x02-functions_nested_loops/_putcar.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the charcter c to stdout 
+ * Oc: the charcter to print
+ *
+ * Return: On success 1.
+ * 	On error. -1 is returned. and errno is set appropriately.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
