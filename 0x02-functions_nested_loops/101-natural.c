183e88777f7303c094eadc0e3094079d38344e7e
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-12 14:18:36 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: computes the sum of all
+ *		the muliple of 3 or 5
+ *		bellow 1024 (excluded)
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; n < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0)
+				sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
