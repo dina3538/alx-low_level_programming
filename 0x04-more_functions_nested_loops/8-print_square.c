4ff8bc386f404cba89728e973837114d402053fc
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-14 11:43:16 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,16 +5,20 @@
  *
  * @size: is the size of the square
  *
- * Return: Always 0 (success)
+ * Return: empty
 */
 
 void print_square(int size)
 {
 	int row, coulmn;
 
-	for (row = 1; row <= size; row++)
+	if (size <= 0)
 	{
-		for (coulmn = 1; coulmn <= size; coulmn++)
+		_putchar('\n');
+	}
+	for (row = 0; row <= size; row++)
+	{
+		for (coulmn = 0; coulmn <= size; coulmn++)
 			_putchar('#');
 		_putchar('\n');
 	}
