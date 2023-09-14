658fda1c374d51bb068d0963d49e7546896c8120
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-14 09:22:41 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,7 +7,7 @@
  * Return: Always 0 (success)
 */
 
-void print_number(void)
+void print_numbers(void)
 {
 	int num = 0;
 
@@ -16,4 +16,4 @@ void print_number(void)
 		num++;
 	} while (num >= 0 && num <= 9);
 	_putchar ('\n');
-}	
+}
