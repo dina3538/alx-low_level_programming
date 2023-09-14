3fd508ca11f84062da3ec72e9884db9c42f8047b
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-14 09:13:02 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#inlude "main.h"
+
+/**
+ * print_numbers - print 0-9
+ *		only use _putchar twise
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_number(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar ('\n');
+}	
