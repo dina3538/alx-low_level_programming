6530d36e27ba608466572eab629594e8febe46df
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-14 11:27:54 -0700

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_square(int size)
+{
+	int row, coulmn;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (coulmn = 1; coulmn <= size; coulmn++)
+		{
+			_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
