e854585e29aed8821d182520a2cb5729eed74a6e
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-14 13:11:20 -0700

    a

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: always 0 (sucess)
+*/
+
+void print_triangle(int size)
+{
+	int hight, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hight = 1; hight <= size; hight++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hight + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+		}
+	}
+}
