46b75bd3097457ec5b6ab81d5e7ce663b3864cef
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-18 11:23:47 -0700

    a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,16 +11,34 @@
 
 int main(void)
 {
-	int sum;
-	char c;
+	int index = 0, sum = 0, diff_half1, diff_half2;
+	char password[84];
 
-	srand(time(NULL));
-	while (sum <= 2645)
+	srand(time(0));
+	while (sum < 2772)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	putchar(2772 - sum);
-	return (0);
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1)
+			{
+				password[index] -= diff_half2;
+
+				break;
+			}
+		}
+	}
 }
