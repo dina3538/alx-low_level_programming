542fb89909908601a44d90b895b98c9d27694a49
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-18 09:55:06 -0700

    a

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - generate randome valid password for the
+ * ptogram 101_crackme.
+ *
+ * Return: always 0.
+*/
+
+int main(void)
+{
+	int num;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
