2aec46f8861e54749b8da58ef95338e788b49312
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-18 11:25:24 -0700

    a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -33,7 +33,7 @@ int main(void)
 
 		for (index = 0; password[index]; index++)
 		{
-			if (password[index] >= (33 + diff_half1)
+			if (password[index] >= (33 + diff_half1))
 			{
 				password[index] -= diff_half2;
 
