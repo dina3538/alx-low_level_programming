f883e78d8b3beb7b4135a37a5b20fc38b4d5632c
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-20 09:38:26 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strncat - concatennates two string  using at most
+ * an inputted number of bytes from src.
+ * @dest: the string to be appended upon.
+ * @src: the string to be appended to dest.
+ * @n: the number of bytes from src to be appended to dest.
+ * Return: a pointer to the resulting string dest.
+*/
+
