afc8fd667b86a3c628b532b3e762d86f2cf75b80
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-20 07:42:12 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatennates two string
+ *
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++);
+		dest[c++] = src[c2];
+
+	return (dest);
+}
