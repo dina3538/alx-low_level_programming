c504190e933b83e98e4a6feb4c903065ffdacb0b
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-21 12:40:57 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,15 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[index])
+	while (str[index] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
+		if (index == 0)
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				str[index] = str[index] - 32;
+			}
+		}
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
@@ -28,9 +32,13 @@ char *cap_string(char *str)
 			str[index - 1] == '(' ||
 			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
-			str[index - 1] == '}' ||
-			index == 0)
-				str[index] -= 32;
+			str[index - 1] == '}')
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				str[index] = str[index] - 32;
+			}
+		}
 		index++;
 	}
 	return (str);
