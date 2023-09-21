916c735c52c8e610e8b104bfd8295ce39e9a8dac
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-21 12:23:58 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,37 @@
 #include "main.h"
-#include<ctype.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to be capitalized
+ * *cap_string - captitalizes all words of a string.
+ * @str: the string to be captitalized
  *
- * Return: pointer to the modified string
+ * Return: str
 */
 
 char *cap_string(char *str)
 {
-	int capitalize = 1; /*Flag to indicate whether to capitalize the next character*/
+	int index = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[index])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-			capitalize = 1;
-		}
-		else if (capitalize)
-		{
-			str[i] = toupper(str[i]);
-			capitalize = 0;
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+				str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
