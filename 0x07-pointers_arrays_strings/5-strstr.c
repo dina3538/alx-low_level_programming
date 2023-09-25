03b5a15515960a520b41f5f6a75590edfb4be7ed
dina3538 <dinaaltayeb1234@gmail.com>
2023-09-25 07:41:37 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: input
+ * @needle: input
+ *
+ * Return: 0 (success)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *s1 = haystack;
+		chsr *s2 = needle;
+
+		while (*s1 == *s2 && *s2 != '\0')
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
