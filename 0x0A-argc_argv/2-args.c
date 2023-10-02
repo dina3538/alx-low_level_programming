3d077f93820fc4b1d3053f80725267d1a7665b5e
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-02 06:05:15 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints all arguments its receivs
+ * @argc: int
+ * @argv: list arguments
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++, argc--)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
