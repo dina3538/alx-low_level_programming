2906433f7c6bed55cd2a1b630492439bab7a9bc6
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-07 08:30:30 -0700

    a

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<string.h>
+#include<stdlib.h>
+
+/**
+ * *_calloc -  a function that allocates memory for an array,
+ * @nmemb: array length
+ * @size: size of each element
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *dd;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	dd = malloc(sizeof(int) * nmemb);
+	if (dd == 0)
+		return (NULL);
+	memset(dd, 0, sizeof(int) * nmemb);
+	return (dd);
+}
