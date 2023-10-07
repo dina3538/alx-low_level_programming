2338fc69ee9c71f5c24265a74f1c61ae50d4a196
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-07 07:42:14 -0700

    a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory
+ * @b: int
+ * Return: pointer or NULL
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+
+		exit(98);
+
+	return (ptr);
+}
