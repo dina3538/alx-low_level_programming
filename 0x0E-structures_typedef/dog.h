6174765070ae0173f597f1d152a5db842b5c01ec
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-10 10:20:39 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: name of the dogs
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: just dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
