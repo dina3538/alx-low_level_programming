e9f831124179389f4463d8f01cd874c65715e689
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-10 12:08:31 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,20 +4,20 @@
 
 /**
  * print_dog - print a dog
- * @d: dog to print
+ * @d: truct dog to print
  * Return: void
 */
 
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (d->name == NULL)
-			d->name = "(nil)";
-		if (d->owner == NULL)
-			d->owner = "(nil)";
-		printf("name: %s\n", d->name);
-		printf("age: %f\n", d->age);
-		printf("owner: %s\n", d->owner);
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("name: %s\n", d->name);
+	printf("age: %f\n", d->age);
+	printf("owner: %s\n", d->owner);
 }
