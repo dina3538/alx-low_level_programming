eacfe15025e508133c8a065f1108a4bd013b3750
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-11 13:20:51 -0700

    a

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - the main func
+ * @argc: num of args
+ * @argv: args vec
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int d;
+
+	if (args != 2)
+		printf("Error\n"), exit(1);
+
+	d = atoi(argv[1]);
+	if (d < 0)
+		printf("Error\n"), exit(2);
+	while (d--)
+		printf("%02hhx%s", *ptr++, d ? " " : "\n");
+	return (0);
+}
