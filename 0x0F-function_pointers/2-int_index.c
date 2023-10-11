6e1dcf156f0c13e685982b042c5386c3007e7bce
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-11 09:42:45 -0700

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "FUNCTION_POINTERS_H"
+
+/**
+ * int_index - search for int
+ * @array: the array
+ * @size: num of elements
+ * @cmp: pointer to func
+ * Return: index
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j = 0;
+
+	if (!array || size <= 0 || !cmp)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]))
+			return (j);
+	}
+	return (-1);
+}
