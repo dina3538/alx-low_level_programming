376643ba9c6f6184f37a14c15fc6b551cdf34a0f
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-11 12:12:23 -0700

    d

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,20 +10,22 @@
 
 int main(int argc, char **argv)
 {
-	int (*op_func)(int, int), a, b;
+	int (*op_func)(int, int), d, e;
 
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	d = atoi(argv[1]);
+	e = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+	exit(99);
 
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(98);
-	printf("%d\n", op_func(a, b));
+	if (!e && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n");
+	exit(100);
+	printf("%d\n", op_func(d, e));
 	return (0);
 }
