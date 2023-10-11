cacd7e36876c4461617574686edae54a1333f2c6
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-11 11:53:52 -0700

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: first int
+ * @b: 2nd int
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: first int
+ * @b: 2nd int
+ * Return: the sub
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - muliply
+ * @a: first int
+ * @b: 2nd int
+ * Return: the mul
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: first int
+ * @b: 2nd int
+ * Return: the div
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module
+ * @a: first int
+ * @b: 2nd int
+ * Return: the module
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
