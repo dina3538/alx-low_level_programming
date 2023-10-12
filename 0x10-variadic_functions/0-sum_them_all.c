c19f3157760884f7b683ae41a270d69c23266a28
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-12 09:57:41 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	int r = 0;
-	int i;
+	int i, x;
 	
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
 	{
-		int x = va_arg(args, int);
+		x = va_arg(args, int);
 
 		r += x;
 
