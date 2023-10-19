2f4140a7028af08a0693994be28ee2cc574fb9ab
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-19 13:23:17 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - function that print a linked list
+ * @h: ponter
+ * Return: num of node
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t j = 0;
+
+	while (h != NULL)
+	{
+		if (h.str != NULL)
+			printf("[%d] %s\n", h.len, h.str);
+		else
+			printf("[0] (nil)\n");
+		h = h.next;
+		j++;
+	}
+	return (j);
+}
+
+
