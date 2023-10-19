9feecb43d03012e9386ab9135af8c95474b4c2d1
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-19 13:40:26 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", strlen(h->str), h->str ? h->str: "(nil)");
+		if (h.str)
+			printf("[%d] %s\n", h.len, h.str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		j++;
 	}
