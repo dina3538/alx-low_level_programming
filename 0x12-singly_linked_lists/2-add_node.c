0c4c8ecf011a415b507d17e98cbae8ed87233dd8
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-19 13:57:20 -0700

    a

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * *add_node - add elem in the begg
+ * @head: ponter to pointer
+ * @str: pointee
+ * Return: the addrs
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *add;
+	unsigned int len = 0;
+
+	while (str[len] != NULL)
+		len++;
+	add = malloc(sizeof(list_t));
+	if (!add)
+		return (NULL);
+
+
+	add->str = strdup(str);
+	add->len = len;
+	add->next = (*head);
+	(*head) = add;
+	return (*head);
+}
