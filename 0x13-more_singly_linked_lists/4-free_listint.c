43d2bbe6aa0ea0df226648d1cd244a08a1e03593
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-23 10:26:47 -0700

    a

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - free
+ * @head: pointer
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *f;
+
+	while ((f = head) != NULL)
+	{
+		head= head->next;
+		free(f);
+	}
+}
