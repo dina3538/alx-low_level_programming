68849d8813b03dc79107bf8c5f6d3fe118cf54d0
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-24 07:07:16 -0700

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (f->next == NULL)
 			node = f->next;
 		else
-			node = f->next;
+			node = f->next->next;
 		free(f->next);
 		f->next = node;
 	}
