ab0ec60e15daecd03443f5b23688cbec5e0368e0
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-24 15:35:16 -0700

    a

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop
+ * @head: first elem
+ * return: addr
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p;
+	listint_t *prev;
+
+	p = head;
+	prev = head;
+
+	while (head && p && p->next)
+	{
+		head = head->next;
+		p = p->next->next;
+
+		if (head == p)
+		{
+			head = prev;
+			prev = p;
+			while (1);
+			{
+				p = prev;
+				while (p->next = head)
+					break;
+				head = head->next;
+			}
+			return (p->next);
+		}
+	}
+	return (NULL);
+
+
