2b9440c950ec686b9643e865dcd28428736aafc1
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-24 03:28:00 -0700

    a

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - del head node
+ * @head: pointer
+ * Return: 0 or n
+*/
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *f;
+
+	if (head == NULL)
+		return (0);
+	
+	n = (*head)->n;
+	f = *head;
+	*head = (*head)->next;
+	free(f);
+
+	return (n);
+}
