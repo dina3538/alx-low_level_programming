e382f748c9558a8c01e7d84cf33c371f0e69158b
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-24 05:56:48 -0700

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -57,7 +57,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		old = f->next;
-		f->next = old;
+		f->next = add;
 		add->next = old;
 	}
 	return (add);
