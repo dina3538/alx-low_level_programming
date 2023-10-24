050b3c98922dad81cd5e0dd68134165190247cc0
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-24 16:37:05 -0700

    a

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+/**
+ * fun_l - fun
+ * @head: firs el
+ *
+ * Return: addr
+ */
+
+lisint_t *fun_l(listint_t *head)
+{
+	listint_t *p, *en;
+
+	if (head == NULL)
+		return (NULL);
+	for (en = head->next; en != NULL; en = en->next)
+	{
+		if (en == en->next)
+			return (en);
+		for (p = head; p != en; p = p->next)
+			if (p == end->next)
+				return (en->next);
+	}
+	return (NULL);
+}
+
+
+/**
+ * free_listint_safe - free
+ * @h: pointer
+ * Return: size
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *next, *lo;
+	size_t length;
+	int loop = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+	lo = fun_l(*h);
+	for (length = 0; (*h != lo || loop ) && *h != NULL; *h = next)
+	{
+		length++;
+		next = (*h)->next;
+		if (*h == lo && loop)
+		{
+			if (lo == lo->next)
+			{
+				free(*h);
+				break;
+			}
+			length++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	returm (length);
+}
+
+
