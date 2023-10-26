0207d8cc90c1d868b6b32e57cddda451d5babf0c
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-26 06:49:00 -0700

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary num
+ * @b: pointer to num
+ * Return: converter num
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int R = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		R = R * 2 +(*b - '0');
+		b++;
+
+	}
+	return (R);
+}
+
