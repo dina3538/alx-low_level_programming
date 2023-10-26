d8ee87e19ceb85ffdae40ddf1830e3d661273a00
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-26 08:26:06 -0700

    a

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip
+ * @n: poin
+ * @m: int
+ * Return: num of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int l;
+
+	while (xor)
+	{
+		if (xor & 1ul)
+			l++;
+		xor = xor >> 1;
+	}
+	return (1);
+}
