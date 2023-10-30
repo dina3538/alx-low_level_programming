8bfcad0d61ab04b91de040cd9fc6ddf5d4e16afa
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-30 06:28:37 -0700

    a

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - create
+ * @filename: name of file
+ * @text_content: the content
+ * Return: 1 in success -1 in fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int dn;
+	ssize_t b;
+
+	if (filename == NULL)
+		return (-1);
+	dn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (dn == -1)
+		return (-1);
+	if (text_content)
+	b = write(dn, text_content, strlen(text_content));
+
+	if (b == -1)
+	{
+		close(dn);
+		return (-1);
+	}
+}
+
