7f88a7c74ef8a931ed6dc31d26b7ed3b4164328a
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-07 09:52:21 -0800

    a

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node 
+ * @head: the head
+ * @n: size
+ * Return: the addrss
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *add = malloc(sizeof(dlistint_t));
+
+	if (!head || !add)
+		return (add ? free(add), NULL : NULL);
+
+	add->n = n;
+	add->prev = NULL;
+	if (!*head)
+	{
+		*head = add;
+		add->next = NULL;
+	}
+	else
+	{
+		add->next = *head;
+		(*head)->prev = add;
+		*head = add;
+	}
+	return (add);
+}
