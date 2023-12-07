dd16429058764ac744b8c8dec150f370af0c5d7e
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-07 02:23:04 -0800

    a

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all element
+ * @h: address
+ * Return: the size
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		print("%d\n", h->n);
+		h = h->next;
+		k++;
+	}
+	return (k);
+}
