0771f17a00446ff7850679823cb07c0bad54003a
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-07 11:55:26 -0800

    a

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,11 @@
-i#include "lists.h"
+#include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
+ * insert_dnodeint_at_index - inserts a new node
+ * @h: head
  * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
+ * @n: valua
+ * Return: the address of the node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
