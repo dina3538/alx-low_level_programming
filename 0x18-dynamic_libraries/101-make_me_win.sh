033ebf3f3c0aa5f2062386fee73cc06a1ba89f71
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-11 04:12:12 -0800

    a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
-wget -P /tmp https:/dina3538/githup.com/dina3538/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+#!/bin/bash
+wget -P /tmp https://github.com/dina3538/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
